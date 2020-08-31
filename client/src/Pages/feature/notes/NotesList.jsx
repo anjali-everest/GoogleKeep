88dfe19562db2d20c746bddf384af60ca73a8ea8
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-08-31 13:23:16 +0530

    Display notes by fetching from the server from Google-Keep database

--- client/src/Pages/feature/notes/NotesList.jsx
+++ client/src/Pages/feature/notes/NotesList.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchNotes } from "./notesSlice"
 
 export const NotesList = () => {
+  const dispatch = useDispatch()
   const notes = useSelector(state => state.notes)
+  const noteStatus = useSelector(state => state.notes.status)
+  const error = useSelector(state => state.notes.error)
 
-  const renderedNotes = notes !== undefined && notes !== 0 && notes.map(note => (
-    <article className="note-excerpt" key={note.id}>
-      <h3>{note.title}</h3>
-      <p>{note.content.substring(0, 100)}</p>
-    </article>
-  ))
+  useEffect(() => {
+    if (noteStatus === 'idle') {
+      dispatch(fetchNotes())
+    }
+  }, [noteStatus, dispatch])
+
+  let content
+  if (noteStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (noteStatus === 'succeeded') {
+    console.log(notes)
+    content = notes.notes.map(note => (
+      <article className="note-excerpt" key={note.id}>
+        <h3>{note.title}</h3>
+        <p>{note.content.substring(0, 100)}</p>
+      </article>
+    ))
+  } else if (noteStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section>
       <h2>Notes</h2>
-      {renderedNotes}
+      {content}
     </section>
   )
 }
--- client/src/Pages/feature/notes/notesSlice.js
+++ client/src/Pages/feature/notes/notesSlice.js
@@ -1,23 +1,42 @@
 import {
-    createSlice
+    createSlice,
+    createAsyncThunk
 } from '@reduxjs/toolkit'
+import {
+    getNotes
+} from "../../../Util/ApiHandler"
+import ResponseHandler from "./../../../Util/ResponseHandler"
 
-const initialState = [{
-        id: '1',
-        title: 'First Note!',
-        content: 'Hello!'
-    },
-    {
-        id: '2',
-        title: 'Second Note',
-        content: 'More text'
-    }
-]
+const initialState = {
+    notes: [],
+    status: 'idle',
+    error: null
+}
+
+export const fetchNotes = createAsyncThunk('notes/fetchNotes', async () => {
+    const response = ResponseHandler.getResponse(await getNotes());
+    console.log(response)
+    return response.data
+})
 
 const notesSlice = createSlice({
     name: 'notes',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [fetchNotes.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [fetchNotes.fulfilled]: (state, action) => {
+            state.status = 'succeeded'
+            // Add any fetched posts to the array
+            state.notes = state.notes.concat(action.payload)
+        },
+        [fetchNotes.rejected]: (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        }
+    }
 })
 
 export default notesSlice.reducer
