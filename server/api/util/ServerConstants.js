cdac8c6afb72e4e106f37907ee00596d4e802bf8
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-09-04 13:07:58 +0530

    Fix addNote, updateNote API issue due to single quote for input string

--- server/api/util/ServerConstants.js
+++ server/api/util/ServerConstants.js
@@ -1,9 +1,9 @@
 //Query constants
-export const GET_ALL_NOTES = 'SELECT * FROM notes';
-export const GET_ONE_NOTE = 'SELECT * FROM notes WHERE id = %d';
-export const INSERT_NOTE = 'INSERT INTO notes(title,content) VALUES (%s,%s)';
-export const UPDATE_NOTE = 'UPDATE notes SET title = %s,content = %s WHERE id = %d';
-export const DELETE_NOTE = 'DELETE FROM notes WHERE id = %d';
+export const GET_ALL_NOTES = "SELECT * FROM notes";
+export const GET_ONE_NOTE = "SELECT * FROM notes WHERE id = %d";
+export const INSERT_NOTE = "INSERT INTO notes(title,content) VALUES ('%s','%s')";
+export const UPDATE_NOTE = "UPDATE notes SET title = '%s',content = '%s' WHERE id = %d";
+export const DELETE_NOTE = "DELETE FROM notes WHERE id = %d";
 //API response message constants
 export const NOTES_RETRIEVED = "Notes retrieved!";
 export const NOTES_NOT_FOUND = "Notes not found!";
