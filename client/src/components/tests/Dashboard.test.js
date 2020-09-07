9127e4e991a8f4a3c04063c4f7e4e8c891ea21a4
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-09-07 18:50:57 +0530

    Setup jest and enzyme and babel support for jest

--- client/src/components/tests/Dashboard.test.js
+++ client/src/components/tests/Dashboard.test.js
@@ -2,12 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import Dashboard from "../Dashboard";
 
-describe("LoginView tests", () => {
+describe("Dashboard tests", () => {
   it("Should render without crashing", () => {
     shallow(<Dashboard />);
-  });
-  it("should match the LoginView snapshot", function () {
-    const dashboard = shallow(<Dashboard props />);
-    expect(dashboard).toMatchSnapshot();
   });
 });
