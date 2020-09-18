e5e3d2299f51c9369adb1450b06c1e9b0177154d
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-09-18 16:41:13 +0530

    Refactor NoteController

--- server/api/controller/notecontroller.js
+++ server/api/controller/notecontroller.js
@@ -1,31 +1,43 @@
+import { NOTE_NOT_FOUND_WITH_ID } from "../util/serverConstants";
+
+let noteService = null;
 export default class NoteController {
-  constructor(noteService) {
-    this.noteService = noteService;
+  constructor(service) {
+    noteService = service;
   }
 
   getAllNotes = async (request, response) => {
-    const notes = await this.noteService.getAll();
-
-    return response.json(notes);
+    try {
+      const notes = await noteService.getAll();
+      return response.json(notes);
+    } catch (error) {
+      return sendError(response, 500, error);
+    }
   };
 
   getOneNote = async (request, response) => {
     const { id } = request.params;
-    const note = await this.noteService.getOne(id);
-    if (!note || note.length === 0)
-      return response.status(400).json({ error: "Note not found with ID" });
-
-    return response.json(note);
+    try {
+      const note = await noteService.getOne(id);
+      if (!isExist(note))
+        return sendError(response, 400, NOTE_NOT_FOUND_WITH_ID);
+      return response.json(note);
+    } catch (error) {
+      return sendError(response, 500, error);
+    }
   };
 
   addNote = async (request, response) => {
-    const { title, content } = request.body;
-    const note = await this.noteService.insert({
-      title,
-      content,
-    });
-
-    response.status(201).json(note);
+    try {
+      const { title, content } = request.body;
+      const note = await noteService.insert({
+        title,
+        content,
+      });
+      response.status(201).json(note);
+    } catch (error) {
+      return sendError(response, 500, error);
+    }
   };
 
   updateNote = async (request, response) => {
@@ -34,21 +46,36 @@ export default class NoteController {
       title: request.body.title,
       content: request.body.content,
     };
-    const noteToUpdate = await this.noteService.getOne(note.id);
-    if (!noteToUpdate || noteToUpdate.length === 0)
-      return response.status(400).json({ error: "Note not found with ID" });
-
-    const updatedFile = await this.noteService.update(note);
-    return response.json(updatedFile);
+    try {
+      const noteToUpdate = await noteService.getOne(note.id);
+      if (!isExist(noteToUpdate))
+        return sendError(response, 400, NOTE_NOT_FOUND_WITH_ID);
+      console.log("exist");
+      const updatedFile = await noteService.update(note);
+      return response.json(updatedFile);
+    } catch (error) {
+      return sendError(response, 500, error);
+    }
   };
 
   deleteNote = async (request, response) => {
     const id = request.params.id;
-    const noteToDelete = await this.noteService.getOne(id);
-    if (!noteToDelete || noteToDelete.length === 0)
-      return response.status(400).json({ error: "Note not found with ID" });
-
-    await this.noteService.deleteOne(id);
-    return response.status(204).json();
+    try {
+      const noteToDelete = await noteService.getOne(id);
+      if (!isExist(noteToDelete))
+        return sendError(response, 400, NOTE_NOT_FOUND_WITH_ID);
+      await noteService.deleteOne(id);
+      return response.status(204).json();
+    } catch (error) {
+      return sendError(response, 500, error);
+    }
   };
 }
+
+const isExist = (item) => {
+  return item && item.length !== 0;
+};
+
+const sendError = (response, status, error) => {
+  return response.status(status).json({ error: error });
+};
--- server/api/validation/validator.js
+++ server/api/validation/validator.js
@@ -1,41 +1,36 @@
 import pkg from "express-validator";
 const { body, param, validationResult } = pkg;
 
-const addNoteValidation = (req, res, next) => {
-  body("title").optional().isString(), body("content").optional().isString();
-  validate(req, res, next);
-};
+export default class Validator {
+  static addNoteValidation = (req, res, next) => {
+    body("title").optional().isString(), body("content").optional().isString();
+    this.validate(req, res, next);
+  };
 
-const getNoteValidation = (req, res, next) => {
-  param("id").not().isEmpty().isInt();
-  validate(req, res, next);
-};
+  static getNoteValidation = (req, res, next) => {
+    param("id").not().isEmpty().isInt();
+    this.validate(req, res, next);
+  };
 
-const updateNoteValidation = (req, res, next) => {
-  param("id").not().isEmpty().isInt(),
-    body("title").optional().isString(),
-    body("content").optional().isString();
+  static updateNoteValidation = (req, res, next) => {
+    param("id").not().isEmpty().isInt(),
+      body("title").optional().isString(),
+      body("content").optional().isString();
 
-  validate(req, res, next);
-};
+    this.validate(req, res, next);
+  };
 
-const deleteNoteValidation = (req, res, next) => {
-  param("id").not().isEmpty().isInt();
-  validate(req, res, next);
-};
+  static deleteNoteValidation = (req, res, next) => {
+    param("id").not().isEmpty().isInt();
+    this.validate(req, res, next);
+  };
 
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
+  validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
 
-  return res.status(422).json(errors);
-};
-
-export {
-  addNoteValidation,
-  getNoteValidation,
-  updateNoteValidation,
-  deleteNoteValidation,
-};
+    return res.status(422).json(errors);
+  };
+}
