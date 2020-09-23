bdddce89e2e5622ae0ef9b40654e5ac6c39a5e46
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-09-23 16:49:35 +0530

    Add env variable to support test database to be present for running tests

--- server/database/index.js
+++ server/database/index.js
@@ -2,11 +2,13 @@ const pg = require("pg");
 require("dotenv").config();
 
 const Pool = pg.Pool;
-let pool;
-const connectionURL = null;
+
+let connectionURL = null;
 if (process.env.ENVIRONMENT === "test")
   connectionURL = process.env.TEST_DATABASE_URL;
 else connectionURL = process.env.DATABASE_URL;
+
+let pool;
 pool = new Pool({
   connectionString: connectionURL,
 });
