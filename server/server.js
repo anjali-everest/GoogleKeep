075fbd903e32d1c606be5362e8a1f5c06beea58e
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-09-23 18:18:14 +0530

    Add jest to be run on seperate port

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const noteRouter = require("./api/routes/NoteRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", noteRouter);
-const server = app.listen(8080);
+const server = app.listen(process.env.PORT || 8080);
 
 module.exports = server;
