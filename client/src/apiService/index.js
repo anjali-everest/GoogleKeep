de63615eee9916ce6a88ffbdbe0ef41bba34f6d3
Gaddam Anjali <anjali.gaddam@everest.engineering>
2020-10-13 13:47:39 +0530

    Remove unnecessory env, baseURL for axios

--- client/src/apiService/index.js
+++ client/src/apiService/index.js
@@ -1,5 +1,5 @@
 import { responseErrorHandler, responseHandler } from "./interceptors";
-import axios from "./axios";
+import axios from "axios";
 
 axios.interceptors.response.use(responseHandler, responseErrorHandler);
 
